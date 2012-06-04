56780a3f43ab4a4e43bb50f7c155bbe70591758a
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-06-04 17:33:23 +0400

    introducing map events

--- Engine.js
+++ Engine.js
@@ -88,7 +88,7 @@ return declare([Engine], {
 		return this.ge;
 	},
 	
-	on: function(feature, event, method, context) {
+	onForFeature: function(feature, event, method, context) {
 		// normalize the callback function
 		method = this.normalizeCallback(feature, event, method, context);
 		var connections,
