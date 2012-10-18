d761a93903a8c8d4fabba8f46d6e7589062a4166
Vladimir Elistratov <vvoovv.projects@gmail.com>
2012-10-18 22:11:19 +0400

    zoom_changed event

--- Navigation.js
+++ Navigation.js
@@ -1,6 +1,12 @@
 define([
 	"dojo/_base/declare"
 ], function(declare) {
+	
+function _onBoundsChange(event) {
+	if (event.get("newZoom") != event.get("oldZoom")) {
+		this._onZoom();
+	}
+}
 
 return declare(null, {
 
@@ -9,9 +15,11 @@ return declare(null, {
 		var ymap = this.map.engine.ymap;
 		if (enable) {
 			ymap.behaviors.enable(["drag", "dblClickZoom", "scrollZoom"]);
+			ymap.events.add('boundschange', _onBoundsChange, this);
 		}
 		else {
 			ymap.behaviors.disable(["scrollZoom", "dblClickZoom", "drag"]);
+			ymap.events.remove('boundschange', _onBoundsChange, this);
 		}
 	}
 });
